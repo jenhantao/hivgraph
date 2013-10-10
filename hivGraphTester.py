82e7655c6267106f82d3f4f247ecf1fd0e97db0b
Jenhan Tao <jenhantao@gmail.com>
2013-10-10 14:15:43 -0700

    added data

---
+++ hivGraphTester.py
@@ -0,0 +1,8 @@
+#null hypothesis is the collapsed tree
+#have to test once for each version of the collapsed tree -> three tests for each triple
+#write a new fasta file each tree variant? or load all sequences into memory
+
+# read in matrix
+
+# generate graph
+
